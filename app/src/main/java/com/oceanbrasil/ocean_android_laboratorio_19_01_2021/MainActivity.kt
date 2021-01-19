ec75a932fc92a2b7428f608110a2e3a45705c38e
Paulo Salvatore <salvatoregames@gmail.com>
2021-01-19 17:03:00 -0300

    Exemplo de recebimento de resultado em uma nova Activity

--- app/src/main/java/com/oceanbrasil/ocean_android_laboratorio_19_01_2021/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_laboratorio_19_01_2021/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.oceanbrasil.ocean_android_laboratorio_19_01_2021
 
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
@@ -21,6 +23,7 @@ class MainActivity : AppCompatActivity() {
         val etNome = findViewById<EditText>(R.id.etNome)
         val btEnviar = findViewById<Button>(R.id.btEnviar)
         val btAbrirResultado = findViewById<Button>(R.id.btAbrirResultado)
+        val btReceberResultado = findViewById<Button>(R.id.btReceberResultado)
 
         btEnviar.setOnClickListener {
             if (etNome.text.isNotBlank()) {
@@ -36,6 +39,27 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra(NOME_DIGITADO, etNome.text.toString())
 
             startActivity(intent)
+        }
+
+        btReceberResultado.setOnClickListener {
+            val intent = Intent(this, ResultActivity::class.java)
+
+            val requestCode = 1
+
+            startActivityForResult(intent, requestCode)
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK && data != null) {
+            val result = data.getStringExtra("RESULTADO")
+
+            val tvResultado = findViewById<TextView>(R.id.tvResultado)
+
+            tvResultado.text = "Resultado Recebido: $result"
         }
     }
 }
--- app/src/main/java/com/oceanbrasil/ocean_android_laboratorio_19_01_2021/ResultActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_laboratorio_19_01_2021/ResultActivity.kt
@@ -1,6 +1,9 @@
 package com.oceanbrasil.ocean_android_laboratorio_19_01_2021
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -14,5 +17,17 @@ class ResultActivity : AppCompatActivity() {
         val textView = findViewById<TextView>(R.id.textView)
 
         textView.text = nomeDigitado
+
+        val btEnviarResultado = findViewById<Button>(R.id.btEnviarResultado)
+
+        btEnviarResultado.setOnClickListener {
+            val intent = Intent()
+
+            intent.putExtra("RESULTADO", "Botão de enviar resultado foi clicado!")
+
+            setResult(Activity.RESULT_OK, intent)
+
+            finish()
+        }
     }
 }
