8bbb04d8ed2c42d9bb23e00a0ec15d9f3f95a2b0
Paulo Salvatore <salvatoregames@gmail.com>
2021-01-19 15:01:57 -0300

    Implementação do etNome atualizando o tvResultado e da validação se o etNome possui informação digitada, exibindo mensagem de erro

--- app/src/main/java/com/oceanbrasil/ocean_android_laboratorio_19_01_2021/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_laboratorio_19_01_2021/MainActivity.kt
@@ -2,6 +2,7 @@ package com.oceanbrasil.ocean_android_laboratorio_19_01_2021
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -12,9 +13,15 @@ class MainActivity : AppCompatActivity() {
 
         val tvResultado = findViewById<TextView>(R.id.tvResultado)
         val btEnviar = findViewById<Button>(R.id.btEnviar)
+        val etNome = findViewById<EditText>(R.id.etNome)
 
         btEnviar.setOnClickListener {
-            tvResultado.text = "Olá, Paulo Salvatore!"
+//            tvResultado.text = "Olá, Paulo Salvatore!"
+            if (etNome.text.isNotBlank()) {
+                tvResultado.text = "Olá, ${etNome.text}!"
+            } else {
+                etNome.error = "Digite um nome..."
+            }
         }
     }
 }
